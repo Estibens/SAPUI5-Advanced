7cf6fa58c7ae993fee9e604366250f15eb2bf204
Estibens <jhon.estib.perez@gmail.com>
2022-08-18 05:08:41 +0000

    First Commit 04

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,29 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function myChech() {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6){
+               // inputEmployee.setDescription("OK");
+
+               this.byId("labelCountry").setVisible(true);
+               this.byId("slCountry").setVisible(true);
+
+            } else {
+               // inputEmployee.setDescription("Not OK");
+
+               this.getView().byId("labelCountry").setVisible(false);
+               this.getView().byId("slCountry").setVisible(false);
+            };
+        }
+
         return Controller.extend("nttdata.employees.controller.MainView", {
             onInit: function () {
 
-            }
+            },
+
+            onValidate: myChech 
         });
     });
